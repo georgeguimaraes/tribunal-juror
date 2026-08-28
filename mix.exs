80833191f2725a6e54943b8994e14a6f5dfc2ed9
George Guimarães <george.guimaraes@gmail.com>
2026-08-28 12:15:06 -0300

    chore: Update application dependencies

--- mix.exs
+++ mix.exs
@@ -40,12 +40,12 @@ defmodule TribunalJuror.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.8.1"},
+      {:phoenix, "~> 1.8.13"},
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 1.1.0"},
-      {:lazy_html, ">= 0.1.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.8.3"},
+      {:phoenix_live_view, "~> 1.2.11"},
+      {:lazy_html, "~> 0.1.12", only: :test},
+      {:phoenix_live_dashboard, "~> 0.9.0"},
       {:esbuild, "~> 0.10", runtime: Mix.env() == :dev},
       {:tailwind, "~> 0.3", runtime: Mix.env() == :dev},
       {:heroicons,
@@ -57,10 +57,10 @@ defmodule TribunalJuror.MixProject do
        depth: 1},
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.26"},
+      {:gettext, "~> 1.0.2"},
       {:jason, "~> 1.2"},
-      {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"},
+      {:dns_cluster, "~> 0.3.0"},
+      {:bandit, "~> 1.12.5"},
 
       # Tribunal LLM evaluation
       {:tribunal, github: "georgeguimaraes/tribunal"},
